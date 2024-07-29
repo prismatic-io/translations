14f79aa90d71991e529eb90747a69b3571d902a3
jpeters5392 <jpeters5392@gmail.com>
2024-07-29 10:13:10 -0400

    Jp/add config wizard translations (#32)
    * Add translation strings for config wizard confirmation modals when replacing write only values
    
    * 1.11.7
    
    ---------
    
    Co-authored-by: Joel Peterson <joel.peterson@prismatic.io>

--- src/lib/shared/configWizard.ts
+++ src/lib/shared/configWizard.ts
@@ -3,9 +3,31 @@ import { SimplePhrase } from "../../types";
 export interface ConfigWizardPhrases {
   /** English: "This configuration page has no configuration." */
   "configWizard.noConfigurationText": SimplePhrase;
+
+  /** English: "By connecting the connection, you will no longer have access to the replaced value. Would you like to continue with the new value? This action cannot be undone." */
+  "configWizard.connect.confirmValueReplacement": SimplePhrase;
+
+  /** English: "By saving this page, you will no longer have access to the replaced value. Would you like to continue with the new value? This action cannot be undone." */
+  "configWizard.submit.confirmValueReplacement": SimplePhrase;
+
+  /** English: "Cancel" */
+  "configWizard.confirmValueReplacement.cancel": SimplePhrase;
+
+  /** English: "Yes, continue" */
+  "configWizard.confirmValueReplacement.confirm": SimplePhrase;
+
+  /** English: "Are you sure?" */
+  "configWizard.confirmValueReplacement.title": SimplePhrase;
 }
 
 export const configWizardPhrases: ConfigWizardPhrases = {
   "configWizard.noConfigurationText":
     "This configuration page has no configuration.",
+  "configWizard.connect.confirmValueReplacement":
+    "By connecting the connection, you will no longer have access to the replaced value. Would you like to continue with the new value? This action cannot be undone.",
+  "configWizard.submit.confirmValueReplacement":
+    "By saving this page, you will no longer have access to the replaced value. Would you like to continue with the new value? This action cannot be undone.",
+  "configWizard.confirmValueReplacement.cancel": "Cancel",
+  "configWizard.confirmValueReplacement.confirm": "Yes, continue",
+  "configWizard.confirmValueReplacement.title": "Are you sure?",
 };
