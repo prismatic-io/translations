dd0814294178e0ff4d130eaa2800f60d50e946da
Joel Peterson <joel.peterson@prismatic.io>
2025-09-08 11:23:23 -0400

    Added translation strings for the reusable connection feature in the config wizard

--- src/lib/shared/oauthConnection.ts
+++ src/lib/shared/oauthConnection.ts
@@ -4,11 +4,19 @@ export interface OAuthConnectionPhrases {
   /** English: "Connect" */
   "oAuthConnection.connectButton": SimplePhrase;
 
+  /** English: "Reconnect" */
+  "oAuthConnection.reconnectButton": SimplePhrase;
+
   /** English: "Disconnect" */
   "oAuthConnection.disconnectButton": SimplePhrase;
+
+  /** English: "Sign in" */
+  "nonOAuthConnection.signInButton": SimplePhrase;
 }
 
 export const oAuthConnectionPhrases: OAuthConnectionPhrases = {
   "oAuthConnection.connectButton": "Connect",
+  "oAuthConnection.reconnectButton": "Reconnect",
   "oAuthConnection.disconnectButton": "Disconnect",
+  "nonOAuthConnection.signInButton": "Sign in",
 };
