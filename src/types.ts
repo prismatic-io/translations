60efb372278311b52ad9e44120c785016edabbe7
Ryan Wersal <ryan.wersal@prismatic.io>
2026-04-10 14:51:27 -0700

    Unroll namespace phrase expansion to optimize typechecking performance

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 import { SharedPhrases } from "./lib/shared";
 import { UniquePhrases } from "./lib/unique";
 
+// When adding a new namespace, update BOTH the enum AND NamespacedSharedPhrases below.
 export enum Namespace {
   COMPONENTS = "components",
   DASHBOARD = "dashboard",
@@ -14,6 +15,24 @@ export enum Namespace {
   MARKETPLACE_INTEGRATION_TEST = "integrations.id.test",
   MARKETPLACE_NOT_FOUND = "app.marketplace-not-found",
 }
+
+type NamespacedAs<N extends string> = {
+  [K in keyof SharedPhrases as `${N}__${string & K}`]: SharedPhrases[K];
+};
+
+// Each entry must correspond to a Namespace enum value above.
+export interface NamespacedSharedPhrases
+  extends NamespacedAs<Namespace.COMPONENTS>,
+    NamespacedAs<Namespace.DASHBOARD>,
+    NamespacedAs<Namespace.INTEGRATIONS>,
+    NamespacedAs<Namespace.MARKETPLACE>,
+    NamespacedAs<Namespace.MARKETPLACE_INTEGRATION_ALERT_MONITORS>,
+    NamespacedAs<Namespace.MARKETPLACE_INTEGRATION_CONFIGURATION>,
+    NamespacedAs<Namespace.MARKETPLACE_INTEGRATION_EXECUTIONS>,
+    NamespacedAs<Namespace.MARKETPLACE_INTEGRATION_LOGS>,
+    NamespacedAs<Namespace.MARKETPLACE_INTEGRATION_SUMMARY>,
+    NamespacedAs<Namespace.MARKETPLACE_INTEGRATION_TEST>,
+    NamespacedAs<Namespace.MARKETPLACE_NOT_FOUND> {}
 
 export type ComplexPhrase<
   T extends Record<string, string | boolean | number> = Record<
@@ -29,10 +48,6 @@ export type ComplexPhrases = PickByType<Phrases, ComplexPhrase>;
 export type SimplePhrase = string;
 
 export type SimplePhrases = PickByType<Phrases, SimplePhrase>;
-
-export type NamespacedSharedPhrases<T extends unknown = SharedPhrases> = {
-  [K in keyof T as `${Namespace}__${string & K}`]: T[K];
-};
 
 export type DynamicPhrases = {
   dynamicPhrase: { [x: string]: string };
